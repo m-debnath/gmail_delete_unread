f0d22d2532f2bf14f28bbb82c76a73175c336078
Mukul Debnath <debnath.mukul@gmail.com>
2022-06-30 03:03:49 +0530

    Use Pandas

--- delete_blocked_threads.py
+++ delete_blocked_threads.py
@@ -2,11 +2,17 @@
 
 from __future__ import annotations, print_function
 
+from typing import Any
+
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from pandas import DataFrame
 from yaml import Loader, load
 
 from gmail_auth import authorize
+
+_black_list: list[str] | None
+_gmail_service: Any
 
 
 def get_black_list() -> list[str] | None:
@@ -26,34 +32,46 @@ def get_black_list() -> list[str] | None:
         return None
 
 
+def check_black_list(row) -> int:
+    global _black_list
+    if _black_list:
+        for each in _black_list:
+            if each.lower() in row["From"].lower():
+                return 1
+    return 0
+
+
+def delete_email_thread(row) -> None:
+    global _gmail_service
+    _gmail_service.users().threads().trash(userId="me", id=row["Thread Id"]).execute()
+
+
 def delete_blocked_threads() -> int:
     """Delete threads with From address matching a black list.
     Return: int
     """
+    global _black_list, _gmail_service
     creds = authorize()
-    black_list = get_black_list()
-    if not black_list:
+    _black_list = get_black_list()
+    if not _black_list:
         input("Press Enter to continue...")
         return 1
-    counter = 0
 
     try:
         # create gmail api client
-        service = build("gmail", "v1", credentials=creds)
+        _gmail_service = build("gmail", "v1", credentials=creds)
 
-        threads = service.users().threads().list(userId="me", q="is:unread").execute().get("threads", [])
+        threads = _gmail_service.users().threads().list(userId="me", q="is:unread").execute().get("threads", [])
+        emails: DataFrame = DataFrame({"Thread Id": [], "From": [], "Text": [], "Spam": []})
         for thread in threads:
-            tdata = service.users().threads().get(userId="me", id=thread["id"]).execute()
+            tdata = _gmail_service.users().threads().get(userId="me", id=thread["id"]).execute()
             msg = tdata["messages"][0]["payload"]
             headers = {header["name"]: header["value"] for header in msg["headers"]}
-            from_address = headers["From"]
-            if black_list:
-                for each in black_list:
-                    if each.lower() in from_address.lower():
-                        service.users().threads().trash(userId="me", id=thread["id"]).execute()
-                        counter += 1
-                        break
-        print(f"{counter} conversation(s) moved to trash.")
+            emails.loc[len(emails)] = [thread["id"], headers["From"], headers["Subject"], 0]
+        emails["Spam"] = emails.apply(lambda row: check_black_list(row), axis=1)
+        spam_emails: DataFrame = emails[emails["Spam"] == 1]
+        spam_emails.apply(lambda row: delete_email_thread(row), axis=1)
+        print(f"{spam_emails.shape[0]} conversation(s) moved to trash.")
         input("Press Enter to continue...")
         return 0
 
