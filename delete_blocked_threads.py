9b615aeb52fa96099629416ec12b3c8733a1f3ac
Mukul Debnath <debnath.mukul@gmail.com>
2022-06-29 23:22:29 +0530

    future annotations

--- delete_blocked_threads.py
+++ delete_blocked_threads.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, annotations
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
